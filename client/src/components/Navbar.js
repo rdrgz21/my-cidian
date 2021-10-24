14f83b4e3044ae3ba5f2a9805261a6060f5f3323
Tom Rodriguez <thomas.rodriguez@sharpgaming.com>
2021-10-24 21:07:41 +0100

    Added Cihui component and tweaked styles

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,27 +2,36 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import NavbarCSS from "./Navbar.module.css" 
 
-export default function Navbar() {
+const Navbar = props => {
+
+  const {studyLang, setStudyLang} = props;
+
+  const isStudyingJapanese = studyLang === 'ja';
+  const isStudyChinese = studyLang === 'zh';
+
   return (
     <nav>
       <h3>Logo</h3>
+      <button onClick={() => setStudyLang(studyLang === 'zh' ? 'ja' : 'zh')}>Switch Lang</button>
       <ul className={NavbarCSS.navLinks}>
         <NavLink exact to="/" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
           <li>Vocab</li>
         </NavLink>
-        <NavLink exact to="/addvocab" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
+        {isStudyingJapanese && <NavLink exact to="/addvocab" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
           <li>Add Vocab</li>
-        </NavLink>
+        </NavLink>}
         {/* <NavLink exact to="/sentences" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
           <li>Sentences</li>
         </NavLink>
         <NavLink exact to="/addsentence" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
           <li>Add Sentence</li>
         </NavLink> */}
-        <NavLink exact to="/addchinese" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
+        {isStudyChinese && <NavLink exact to="/addchinese" className={NavbarCSS.navLink} activeClassName={NavbarCSS.navLinkActive}>
           <li>Add Chinese</li>
-        </NavLink>
+        </NavLink>}
       </ul>
     </nav>
   );
 }
+
+export default Navbar;
