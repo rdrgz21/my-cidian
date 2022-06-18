0c6a0a147363e014d1202516bef34b5795d4c614
thomas.rodriguez <thomas.rodriguez@sharpgaming.com>
2022-06-18 11:32:06 +0100

    Disabled StageBar buttons when necessary data is unnecessary

--- client/src/components/Chinese/AddChinese/StageBar/StageBar.js
+++ client/src/components/Chinese/AddChinese/StageBar/StageBar.js
@@ -6,28 +6,50 @@ export const StageBar = props => {
 
     const {state, dispatch} = useContext(AddChineseContext);
 
-    const {stage} = state;
+    const {stage, readings, pinyin, characters} = state;
 
     const handleClick = e => {
         dispatch({type: CHINESE_ACTIONS.SET_STAGE, payload: parseInt(e.target.value)});
     }
 
+    const readingsAvailable = readings.length > 0;
+    const tonesAvailable = pinyin.length > 0;
+    const reviewAvailable = characters.length > 0 && readingsAvailable && tonesAvailable;
+
     return (
         <div className={StageBarCSS.container}>
             <div>
-                <button className={stage === 1 ? StageBarCSS.activeButton : StageBarCSS.button} onClick={handleClick} value={1}></button>
+                <button 
+                    className={stage === 1 ? StageBarCSS.activeButton : StageBarCSS.button} 
+                    onClick={handleClick} 
+                    value={1}></button>
                 <span>中英</span>
             </div>
             <div>
-                <button className={stage === 2 ? StageBarCSS.activeButton : StageBarCSS.button} onClick={handleClick} value={2}></button>
+                <button 
+                    className={stage === 2 ? StageBarCSS.activeButton : StageBarCSS.button} 
+                    style={!readingsAvailable ? {backgroundColor: 'grey'} : {}} 
+                    onClick={handleClick} 
+                    disabled={!readingsAvailable}
+                    value={2}></button>
                 <span>拼音</span>
             </div>
             <div>
-                <button className={stage === 3 ? StageBarCSS.activeButton : StageBarCSS.button} onClick={handleClick} value={3}></button>
+                <button 
+                    className={stage === 3 ? StageBarCSS.activeButton : StageBarCSS.button} 
+                    style={!tonesAvailable ? {backgroundColor: 'grey'} : {}} 
+                    onClick={handleClick} 
+                    disabled={!tonesAvailable}
+                    value={3}></button>
                 <span>声调</span>
             </div>
             <div>
-                <button className={stage === 4 ? StageBarCSS.activeButton : StageBarCSS.button} onClick={handleClick} value={4}></button>
+                <button 
+                    className={stage === 4 ? StageBarCSS.activeButton : StageBarCSS.button} 
+                    style={!reviewAvailable ? {backgroundColor: 'grey'} : {}} 
+                    onClick={handleClick} 
+                    disabled={!reviewAvailable}
+                    value={4}></button>
                 <span>复习</span>
             </div>
        </div>
